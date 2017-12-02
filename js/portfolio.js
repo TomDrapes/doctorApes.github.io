e15c5d8a7d133c80fcdf1467958343fc8179c917
Tom Drapes <tdrapes@myune.edu.au>
2017-12-02 17:24:13 +1000

    created image slide portfolio section

---
+++ js/portfolio.js
@@ -0,0 +1,34 @@
+$(document).ready(function (){
+    var currentImage = 0;
+
+    $("#left").on("click", function(){
+        if(currentImage === 0){
+            currentImage = 3;            
+        }else{
+            currentImage -= 1;
+        }
+        changeImage(currentImage);
+    });
+    $("#right").on("click", function(){
+        if(currentImage === 3){
+            currentImage = 0;            
+        }else{
+            currentImage += 1;
+        }
+        changeImage(currentImage);
+    });
+
+    function changeImage(current){
+        switch(current){
+            case 0: $("#portfolio").css("background", "url('img/calculator.png')");
+            break;
+            case 1: $("#portfolio").css("background", "url('img/simon.png')");
+            break;
+            case 2: $("#portfolio").css("background", "url('img/tictactoe.png')");
+            break;
+            case 3: $("#portfolio").css("background", "url('img/pomodoro.png')");
+            break;
+            
+        }
+    }
+});
